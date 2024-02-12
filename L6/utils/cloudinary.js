ca6e7c9ac2542bad9a8c9fb35938cd2791a60bbc
Keyur Ajudiya <keyurajudiya31@gmail.com>
2024-02-12 21:21:39 +0530

    File upload system

---
+++ L6/utils/cloudinary.js
@@ -0,0 +1,30 @@
+import { v2 as cloudinary } from 'cloudinary';
+import { response } from 'express';
+import fs from "fs"; // Here "fs" means "filesystem"
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+    try {
+        if (!localFilePath) return null
+
+        //uplod the file on cloudinary
+        cloudinary.uploader.upload(localFilePath, {
+            resource_type: "auto"
+        })
+
+        //if file has been uploaded successfully then console log
+        console.log("file is uploaded in cloudinary", response.url);
+        return response; 
+
+    } catch (error) {
+        fs.unlinkSync(localFilePath) //remove the locally saved temporary file as the upload operation got failed
+        return null;
+    }
+}
+
+export {uploadOnCloudinary}
